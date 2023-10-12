c5316a23d3d488759b263bf6c6aa31216afbbf58
maxprocess <guilhermegrijo@maxprocess.com>
2023-10-12 10:50:42 -0300

    Singleton para buscar token

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,6 +2,11 @@ import SwiftUI
 import shared
 
 struct ComposeView: UIViewControllerRepresentable {
+    
+    init() {
+        print("Token -> \(Singleton.shared.getToken())")
+    }
+    
     func makeUIViewController(context: Context) -> UIViewController {
         Main_iosKt.MainViewController()
     }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,11 +2,33 @@ import SwiftUI
 import shared
 import Firebase
 
+class Singleton {
+    
+    private var fcmToken : String = ""
+    
+    static var shared: Singleton = {
+        let instance = Singleton()
+        // ... configure the instance
+        // ...
+        return instance
+    }()
+    
+    private init() {}
+    
+    
+    func setToken(token : String?) {
+        fcmToken = token ?? ""
+    }
+    func getToken() -> String {
+        return fcmToken
+    }
+}
+
 @main
 struct iOSApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     init() {
-        HelperKt.doInitKoin()
+        HelperKt.koin()
     }
     
     var body: some Scene {
@@ -50,7 +72,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         completionHandler(UIBackgroundFetchResult.newData)
     }
     
-
+    
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         
@@ -67,30 +89,32 @@ extension AppDelegate: MessagingDelegate {
     // Guardando Token
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        Singleton.shared.setToken(token: fcmToken)
         print(dataDict)
     }
 }
 
 // User Notifications ( In app notifications )
 extension AppDelegate: UNUserNotificationCenterDelegate {
-  // Receive displayed notifications for iOS 10 devices.
-  func userNotificationCenter(_ center: UNUserNotificationCenter,
-                              willPresent notification: UNNotification) async
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async
     -> UNNotificationPresentationOptions {
-    let userInfo = notification.request.content.userInfo
-
+        let userInfo = notification.request.content.userInfo
+        
         // Fazer algo com a informaçao
-    print(userInfo)
-
-    // Change this to your preferred presentation option
+        print(userInfo)
+        
+        // Change this to your preferred presentation option
         return [[.banner, .badge, .sound]]
-  }
-
-  func userNotificationCenter(_ center: UNUserNotificationCenter,
-                              didReceive response: UNNotificationResponse) async {
-    let userInfo = response.notification.request.content.userInfo
-
-    // Print full message.
-    print(userInfo)
-  }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        
+        // Print full message.
+        print(userInfo)
+    }
 }
+
